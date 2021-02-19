0dad8b61b5a8dceb3d349c08a20fd2a9acb526b9
hashcodepk <hashirlives@gmail.com>
2021-02-19 17:39:28 +0500

    Container pattern

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,67 +2,39 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 
-import CollectionsOverview from "../../components/colections-overview/collections-overview.component";
-import CollectionPage from "../collection/collection.component";
-import {
-  firestore,
-  convertCollectionsSnapshotToMap,
-} from "../../firebase/firebase.utils";
-import Spinner from "../../components/with-spinner/with-spinner.component";
-import { updateCollections } from "../../redux/shop/shop.actions";
+import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
 
-const CollectionsOverviewWithSpinner = Spinner(CollectionsOverview);
-const CollectionPageWithSpinner = Spinner(CollectionPage);
+import CollectionsOverviewContainer from "../../components/colections-overview/collections-overview.container";
+import CollectionPageContainer from "../collection/collection.container";
 
 class ShopPage extends React.Component {
-  state = {
-    isLoading: true,
-  };
-
-  unsubscribeFromSnapShot = null;
-
   componentDidMount() {
-    const { updateCollections } = this.props;
-    const collectionRef = firestore.collection("collections");
-    this.unsubscribeFromSnapShot = collectionRef.onSnapshot(
-      async (snapshot) => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        updateCollections(collectionsMap);
-        this.setState({ isLoading: false });
-      }
-    );
+    const { fetchCollectionsStartAsync } = this.props;
+
+    fetchCollectionsStartAsync();
   }
 
   render() {
     const { match } = this.props;
-    const { isLoading } = this.state;
+
     return (
-      <div>
-        <div className="shop-page">
-          <Route
-            exact
-            path={`${match.path}`}
-            render={(props) => (
-              <CollectionsOverviewWithSpinner
-                isLoading={isLoading}
-                {...props}
-              />
-            )}
-          />
-          <Route
-            path={`${match.path}/:collectionId`}
-            render={(props) => (
-              <CollectionPageWithSpinner isLoading={isLoading} {...props} />
-            )}
-          />
-        </div>
+      <div className="shop-page">
+        <Route
+          exact
+          path={`${match.path}`}
+          component={CollectionsOverviewContainer}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPageContainer}
+        />
       </div>
     );
   }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  updateCollections: (collectionsMap) =>
-    dispatch(updateCollections(collectionsMap)),
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
 });
+
 export default connect(null, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,33 @@
 import ShopActionTypes from "./shop.types";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
 
-export const updateCollections = (collectionsMap) => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
-  payload: collectionsMap,
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
 });
+
+export const fetchCollectionsSuccess = (collectionMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionMap,
+});
+
+export const fetchCollectionsFailure = (errormessage) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errormessage,
+});
+
+export const fetchCollectionsStartAsync = () => {
+  return (dispatch) => {
+    const collectionRef = firestore.collection("collections");
+    dispatch(fetchCollectionsStart());
+    collectionRef
+      .get()
+      .then((snapshot) => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch((err) => dispatch(fetchCollectionsFailure(err.message)));
+  };
+};
